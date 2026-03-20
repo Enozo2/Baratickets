349653855606583d8fddcf6f5f655e2c0e437d77
javiersolikobe-spec <javiersolikobe@gmail.com>
2026-03-20 00:32:41 -0400

    agregue un comentario

--- Baratickets2.0/Baratickets2.0/Controllers/EventosController.cs
+++ Baratickets2.0/Baratickets2.0/Controllers/EventosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Rotativa.AspNetCore;
+// teo la cabra 
 
 namespace Baratickets2._0.Controllers
 {
